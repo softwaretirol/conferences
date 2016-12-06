820491dbddd757991f8010d2fc62983ff89ab017
Christian Giesswein <cgiesswein@live.com>
2016-12-06 19:13:21 +0100

    DDC 2016

---
+++ DDC 2016/Text... überall nur Text/ConsoleApplication9/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "Hallo";
+            string b = "hallo";
+
+
+            var watch = Stopwatch.StartNew();
+            for (int i = 0; i < 1000000; i++)
+            {
+                if (a.ToLower() == b.ToLower())
+                {
+
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("ToLower: " + watch.Elapsed);
+
+            watch = Stopwatch.StartNew();
+            for (int i = 0; i < 1000000; i++)
+            {
+                if (a.ToUpper() == b.ToUpper())
+                {
+
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("ToUpper: " + watch.Elapsed);
+
+            watch = Stopwatch.StartNew();
+            for (int i = 0; i < 1000000; i++)
+            {
+                //CultureInfo.CurrentCulture
+                //Thread.CurrentThread.cult
+                if (a.Equals(b, StringComparison.CurrentCultureIgnoreCase))
+                {
+
+                }
+            }
+            string.Concat("Hallo", "Welt", "Sepp");
+            watch.Stop();
+            Console.WriteLine("Equals: " + watch.Elapsed);
+
+
+            //string s = "";
+            //string s2 = string.Empty;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Lieber Sepp - Gib dein Passwort ein:");
+            string pw = Console.ReadLine();
+
+            Console.ReadLine();
+
+
+
+
+
+            var totallysecret = "DAS IST ABSOLUT GEHEIM - Geht di nix an";
+
+            var s1 = "hallo";
+            var s2 = "hallo";
+
+            bool isEqual = ReferenceEquals(s1, s2);
+
+            var inputText = "hallo";
+
+            //var methods = inputText.GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Static);
+            var method = inputText.GetType().GetMethod("FillStringChecked", BindingFlags.NonPublic | BindingFlags.Static);
+            method.Invoke(null, new object[] { inputText, 0, char.ToUpper(inputText[0]).ToString() });
+            Console.WriteLine(inputText);
+
+            string myText = "hallo";
+            Console.WriteLine(myText);
+
+            //fixed (char* pStr = inputText)
+            //{
+            //    pStr[0] = char.ToUpper(pStr[0]);
+            //}
+            //Console.WriteLine(inputText);
+
+            //char firstLetter = char.ToUpper(inputText[0]);
+            //string result = firstLetter.ToString();
+            //for (int i = 1; i < inputText.Length; i++)
+            //{
+            //    result += inputText[i];
+            //}
+            //Console.WriteLine(result);
+
+            //string s = "Ösi";
+            //var buffer =Encoding.UTF8.GetBytes(s);
+            //Console.WriteLine(buffer.Length);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DDC 2016/WPF DataBinding/DDCSample/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DDCSample.Annotations;
+
+namespace DDCSample
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var binding = new Binding();
+            //BindingExpression bindingExpression = (BindingExpression)tb.SetBinding(TextBlock.TextProperty, binding);
+
+            //bindingExpression.UpdateTarget();
+        }
+    }
+
+    public class MyMarkupExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var target = serviceProvider.GetService(typeof(IProvideValueTarget)) as IProvideValueTarget;
+            //target.
+
+            return "Hallo Welt!";
+        }
+    }
+
+
+    public class MyControl : Control
+    {
+
+
+        public int Number
+        {
+            get { return (int)GetValue(NumberProperty); }
+            set { SetValue(NumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty NumberProperty =
+            DependencyProperty.Register("Number", typeof(int), typeof(MyControl), new PropertyMetadata(0));
+
+
+    }
+
+    // PropertyDescriptor
+    // INotifyPropertyChanged - Reflection - PropertyInfo
+    // DependencyProperty
+    public class Person : INotifyPropertyChanged
+    {
+        private int number;
+        private int fastNumber = 42;
+        private int slowNumber = 1024;
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+
+        //BindingSynchronization <-- .NET 4.5
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                if (value == number) return;
+                number = value;
+                OnPropertyChanged(nameof(Number));
+            }
+        }
+
+        public int FastNumber
+        {
+            get { return fastNumber; }
+            set { fastNumber = value; }
+        }
+
+        public int SlowNumber
+        {
+            get
+            {
+                Thread.Sleep(5000);
+                return slowNumber;
+            }
+            set { slowNumber = value; }
+        }
+
+        public Person()
+        {
+            //Task.Run(() =>
+            //{
+            //    while (true)
+            //    {
+            //        Number++;
+            //    }
+            //});
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+
+    public class PropertyChangedBinding : Binding
+    {
+        public PropertyChangedBinding()
+        {
+            this.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+        }
+
+        public PropertyChangedBinding(string path) :
+            base(path)
+        {
+            this.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+
+        }
+    }
+}
