cca30c0822b1ab02765fa42f46b82866655f8045
Christian Giesswein <cgiesswein@live.com>
2016-12-07 08:53:43 +0100

    Added

---
+++ DDC 2016/ArchitekturWorkshop/Workshop/Sample1.cs
@@ -0,0 +1,11 @@
+// Darf nicht geändert werden.
+public interface IShareProvider
+{
+    IEnumerable<IShare> Provide();
+}
+
+
+// Bitte erweitern um notwendige Eigenschaften/Methoden
+public interface IShare
+{
+}
