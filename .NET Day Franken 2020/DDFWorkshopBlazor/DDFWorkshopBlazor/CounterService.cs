71149f3ed4e8d68fdf2e65e89c2149c1fe1d4278
Christian Giesswein <christian@software.tirol>
2020-09-11 13:23:26 +0200

    Zwischenstand Blazor Workshop DDF2020

---
+++ .NET Day Franken 2020/DDFWorkshopBlazor/DDFWorkshopBlazor/CounterService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDFWorkshopBlazor
+{
+    public class CounterService
+    {
+        public int Counter { get; private set; }
+        public event Action CounterChanged;
+        public void Increase()
+        {
+            Counter++;
+            CounterChanged?.Invoke();
+        }
+    }
+}
