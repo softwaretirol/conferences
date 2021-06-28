1c0da391b165d1d931a3907e4d8ca978a8d29d0c
Christian Giesswein <christian@software.tirol>
2021-06-28 12:56:34 +0200

    Workshop

---
+++ DWX2021/Workshop - Blazor/DwxBlazor.Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Starte Server");
+
+Host.CreateDefaultBuilder()
+    .ConfigureWebHostDefaults(x =>
+    {
+        x.UseStartup<Startup>();
+    })
+    .Build()
+    .Run();
+
+
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddServerSideBlazor();
+        serviceCollection.AddRazorPages();
+    }
+
+    public void Configure(IConfiguration configuration,
+        IApplicationBuilder appBuilder)
+    {
+        appBuilder.UseStaticFiles();
+        appBuilder.UseRouting();
+        appBuilder.UseEndpoints(x =>
+        {
+            x.MapBlazorHub();
+            x.MapFallbackToPage("/_Host");
+        });
+    }
+}
