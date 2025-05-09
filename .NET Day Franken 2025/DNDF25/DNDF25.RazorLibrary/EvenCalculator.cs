6a698e64a45d0488940ad60610d5c60c7e21d4df
Christian Giesswein <christian@software.tirol>
2025-05-09 21:14:44 +0200

    DDF2025 Workshop

---
+++ .NET Day Franken 2025/DNDF25/DNDF25.RazorLibrary/EvenCalculator.cs
@@ -0,0 +1,20 @@
+﻿namespace DNDF25.Components
+{
+    public class EvenCalculator
+    {
+        public static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static string IsEvenColor(int i)
+        {
+            return IsEven(i) ? "color: green" : "color: red";
+        }
+
+        public static string IsEvenColorClass(int i)
+        {
+            return IsEven(i) ? "even" : "odd";
+        }
+    }
+}
---
+++ .NET Day Franken 2025/DNDF25/DNDF25.Wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DNDF25.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
