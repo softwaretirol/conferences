3c00a08e4fbf910856486eff824514bb3eadc214
Christian Giesswein <christian@software.tirol>
2025-05-11 11:08:15 +0200

    Blazor Performance DDF2025 - a last time....

---
+++ .NET Day Franken 2025/Blazor/BlazorPerf/BlazorPerf.Components/Pages/MyComponent.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.RenderTree;
+
+namespace BlazorPerf.Components.Pages;
+
+public class MyComponent : IComponent
+{
+    private RenderHandle _renderHandle;
+
+    public void Attach(RenderHandle renderHandle)
+    {
+        _renderHandle = renderHandle;
+        RenderNow(renderHandle);
+    }
+
+    private void RenderNow(RenderHandle renderHandle)
+    {
+        RenderFragment fragment = builder =>
+        {
+            builder.OpenElement(1, "p");
+            builder.AddAttribute(3, "onclick", EventCallback.Factory.Create(this, OnClick));
+            builder.AddMarkupContent(2, "Hello World ");
+            builder.AddMarkupContent(4, DateTime.Now.ToShortDateString());
+            // builder.OpenComponent<Counter>();
+            builder.CloseElement();
+            
+        };
+        // RenderTreeBuilder builder = new();
+        // builder.AddContent(1, fragment);
+        // ArrayRange<RenderTreeFrame> frames = builder.GetFrames();
+        renderHandle.Render(fragment);
+    }
+
+    private void OnClick()
+    {
+        RenderNow(_renderHandle);
+    }
+
+    public Task SetParametersAsync(ParameterView parameters)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ .NET Day Franken 2025/Blazor/BlazorPerf/BlazorPerf/BlazorPerf/Program.cs
@@ -0,0 +1,23 @@
+using BlazorPerf.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+app.UseWebAssemblyDebugging();
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Routes).Assembly);
+
+app.Run();
