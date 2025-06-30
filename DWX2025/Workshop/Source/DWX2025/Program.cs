0b280ba08842cfd4bd21398ef704aacb18cc131a
Christian Giesswein <christian@software.tirol>
2025-06-30 17:59:56 +0200

    Workshop DWX2025

---
+++ DWX2025/Workshop/Source/DWX2025/Program.cs
@@ -0,0 +1,30 @@
+using DWX2025.Components;
+using DWX2025.RCL.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App2>()
+    .AddInteractiveServerRenderMode()
+    .AddAdditionalAssemblies(typeof(Routes).Assembly);
+
+app.Run();
